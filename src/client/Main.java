9af73fb1ec08c3cfcb214cdcc77268baf36bd2c2
Haowen You <hy9be@virginia.edu>
2015-08-12 23:51:15 -0400

    Remove Instrumenters that are not used anymore

--- src/client/Main.java
+++ src/client/Main.java
@@ -13,7 +13,6 @@ public class Main {
         String testFilePath = "/Users/hyou/hyou_1tier_view/BIWebApp/code/html/MSTRWeb/javascript/mojo/js/source/DocModel.js";
 
         SourceProcessor sourceProcessor = new SourceProcessor(new ConfigurationCommon(), "test.js");
-        //ParseTreeInstrumenter instrumenter = (ParseTreeInstrumenter) ReflectionUtils.getField(sourceProcessor, "instrumenter");
 
         //String source = "this.someFn()\n    ._renderItem = function() {};";
 
