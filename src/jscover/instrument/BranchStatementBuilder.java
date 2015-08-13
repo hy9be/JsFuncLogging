15d19cf5f04e8e9075a43d9a5de892b8b316596a
Haowen You <hy9be@virginia.edu>
2015-08-12 23:56:05 -0400

    Remove unnecessary functions in BranchStatementBuilder

--- src/jscover/instrument/BranchStatementBuilder.java
+++ src/jscover/instrument/BranchStatementBuilder.java
@@ -8,8 +8,6 @@ import org.mozilla.javascript.ast.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.lang.String.format;
-
 public class BranchStatementBuilder {
 
     public ExpressionStatement buildLineAndConditionInitialisation(String uri, int lineNo, int conditionNo, int position, int length, String source) {
@@ -80,63 +78,5 @@ public class BranchStatementBuilder {
         NumberLiteral lineNumberLiteral = new NumberLiteral();
         lineNumberLiteral.setValue("'" + lineNo + "'");
         return new ElementGet(branchProperty, lineNumberLiteral);
-    }
-
-    public FunctionNode buildBranchRecordingFunction(String uri, int id, int lineNo, int conditionNo) {
-        Name functionName = new Name();
-        functionName.setIdentifier(format("visit%d_%d_%d", id, lineNo, conditionNo));
-        FunctionNode functionNode = new FunctionNode();
-        functionNode.setFunctionName(functionName);
-
-        Name resultName = new Name();
-        resultName.setIdentifier("result");
-        functionNode.addParam(resultName);
-
-        Block block = new Block();
-        block.addStatement(buildLineAndConditionCall(uri, lineNo, conditionNo));
-
-        ReturnStatement returnStatement = new ReturnStatement();
-        returnStatement.setReturnValue(resultName);
-        block.addChild(returnStatement);
-        functionNode.setBody(block);
-        return functionNode;
-    }
-
-    public FunctionNode buildMstrLoggingFunction(String uri, int id, int lineNo) {
-
-        Name functionName = new Name();
-        functionName.setIdentifier(format("visit%d_%d_%d", id, lineNo));
-        FunctionNode functionNode = new FunctionNode();
-        functionNode.setFunctionName(functionName);
-
-        Name resultName = new Name();
-        resultName.setIdentifier("result");
-        functionNode.addParam(resultName);
-
-        Block block = new Block();
-        //block.addStatement(buildFunctionLoggingCall());
-
-        ReturnStatement returnStatement = new ReturnStatement();
-        returnStatement.setReturnValue(resultName);
-        block.addChild(returnStatement);
-        functionNode.setBody(block);
-        return functionNode;
-    }
-
-    private ExpressionStatement buildFunctionLoggingCall(String uri, int lineNo) {
-
-        Name resultName = new Name();
-        resultName.setIdentifier("result");
-
-        FunctionCall fnCall = new FunctionCall();
-        Name propertyName = new Name();
-        propertyName.setIdentifier("lineNumber");
-        //PropertyGet propertyGet = new PropertyGet(1, propertyName);
-        //fnCall.setTarget(propertyGet);
-        List<AstNode> arguments = new ArrayList<AstNode>();
-        arguments.add(resultName);
-        fnCall.setArguments(arguments);
-
-        return new ExpressionStatement(fnCall);
     }
 }
